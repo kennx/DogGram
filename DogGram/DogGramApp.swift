62959f9d70f3d18be503a466b60e72f188065906
zhangjian <zhangxjian@me.com>
2021-03-09 15:40:49 +0800

    Initial Commit

---
+++ DogGram/DogGramApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogGramApp.swift
+//  DogGram
+//
+//  Created by 张剑 on 2021/3/9.
+//
+
+import SwiftUI
+
+@main
+struct DogGramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
