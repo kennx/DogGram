e1fbc3b6af15a7c071f74302e6bc9c37d1abfb42
zhangjian <zhangxjian@me.com>
2021-03-09 16:15:43 +0800

    first

---
+++ DogGram/Helpers/Extensions.swift
@@ -0,0 +1,29 @@
+//
+//  Extensions.swift
+//  DogGram
+//
+//  Created by 张剑 on 2021/3/9.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+  
+  struct MyTheme {
+    
+    static var purpleColor: Color {
+      return Color("ColorPurple")
+    }
+    
+    static var yellowColor: Color {
+      return Color("ColorYellow")
+    }
+    
+    static var beigeColor: Color {
+      return Color("ColorBeige")
+    }
+    
+  }
+  
+}
--- DogGram/Views/Screens/ContentView.swift
+++ DogGram/Views/Screens/ContentView.swift
@@ -9,8 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
   var body: some View {
-    Text("Hello, world!")
-      .padding()
+    TabView {
+      Text("Screen 1")
+        .tabItem {
+          Image(systemName: "book.fill")
+          Text("Feed")
+        }
+      Text("Screen 2")
+        .tabItem {
+          Image(systemName: "magnifyingglass")
+          Text("Browse")
+        }
+      Text("Screen 3")
+        .tabItem {
+          Image(systemName: "square.and.arrow.up.fill")
+          Text("Upload")
+        }
+      Text("Screen 4")
+        .tabItem {
+          Image(systemName: "person.fill")
+          Text("Profile")
+        }
+    }
+    .accentColor(Color.MyTheme.purpleColor)
   }
 }
 
