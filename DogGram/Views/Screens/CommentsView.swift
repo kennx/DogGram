58c4cff9a640c698451443b9ac237333d05124e4
zhangjian <zhangxjian@me.com>
2021-03-09 17:29:30 +0800

    p8

--- DogGram/Views/Screens/CommentsView.swift
+++ DogGram/Views/Screens/CommentsView.swift
@@ -8,17 +8,51 @@
 import SwiftUI
 
 struct CommentsView: View {
+  
+  @State var submissionText: String = ""
+  
   var body: some View {
     VStack {
-      ScrollView {
+      
+      ScrollView(.vertical, showsIndicators: false) {
+        Text("Hello World")
+        Text("Hello World")
+        Text("Hello World")
+        Text("Hello World")
+        Text("Hello World")
+      }
+      
+      HStack {
+        Image("dog1")
+          .resizable()
+          .scaledToFill()
+          .frame(width: 40, height: 40, alignment: .center)
+          .cornerRadius(20)
+        
+        TextField("Add a comment here...", text: self.$submissionText)
+        
+        Button(action: {
+          
+        }) {
+          Image(systemName: "paperplane.fill")
+            .font(.title2)
+        }
+        .accentColor(Color.MyTheme.purpleColor)
+        
         
       }
+      .padding(.all, 6)
+      
     }
+    .navigationTitle("Comments")
+    .navigationBarTitleDisplayMode(.inline)
   }
 }
 
 struct CommentsView_Previews: PreviewProvider {
   static var previews: some View {
-    CommentsView()
+    NavigationView {
+      CommentsView()
+    }
   }
 }
